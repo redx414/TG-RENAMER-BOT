01c28e41c8d01e10f06ddad8d25133de4a0d9c31
Malan Himsara <87243006+redx414@users.noreply.github.com>
2022-06-16 07:01:11 +0530

    Update 1.py

--- renamer/plugins/1.py
+++ renamer/plugins/1.py
@@ -14,17 +14,17 @@ async def force_sub(c, m):
         try:
             chat = await c.get_chat_member(Config.FORCE_SUB, m.from_user.id)
             if chat.status=='kicked':
-                return await m.reply_text('Hai you are kicked from my updates channel. So, you are not able to use me',  quote=True)
+                return await m.reply_text("You have been kicked from our updates channel. So, you can't use me",  quote=True)
 
         except UserNotParticipant:
-            button = [[InlineKeyboardButton('join Updates channel', url=f'https://t.me/{Config.FORCE_SUB}')]]
+            button = [[InlineKeyboardButton('Join our updates channel', url=f'https://t.me/{Config.FORCE_SUB}')]]
             markup = InlineKeyboardMarkup(button)
             return await m.reply_text(text="Hey join in my updates channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
 
         except ChatAdminRequired:
-            logger.warning(f"Make me admin in @{Config.FORCE_SUB}")
+            logger.warning(f"Make me an admin in @{Config.FORCE_SUB}")
             if m.from_user.id in Config.AUTH_USERS:
-                return await m.reply_text(f"Make me admin in @{Config.FORCE_SUB}")
+                return await m.reply_text(f"Make me an admin in @{Config.FORCE_SUB}")
 
         except UsernameNotOccupied:
             logger.warning("The forcesub username was Incorrect. Please give the correct username.")
@@ -37,7 +37,7 @@ async def force_sub(c, m):
                 if m.from_user.id in Config.AUTH_USERS:
                     return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
             logger.error(e)
-            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [our group](https://t.me/Ns_Bot_supporters)", disable_web_page_preview=True, quote=True)
+            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [our group](https://t.me/RedX14)", disable_web_page_preview=True, quote=True)
 
     await m.continue_propagation()
 
